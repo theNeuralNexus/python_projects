7e8ce548da34bda9d0d6f66018d95dd79901ae33
abs <abdulsalam.network@gmail.com>
2025-06-07 14:16:14 +0500

    Add new feauture

--- temprature_converter/temprature.py
+++ temprature_converter/temprature.py
@@ -3,7 +3,6 @@
 def main() -> None:
     print("\n===============Temprature Converter===============")
 
-
     while True:
         try:
             temprature_in_celsius = float(input("\nEnter temprature in Celsius: "))
@@ -11,12 +10,20 @@ def main() -> None:
             print("\n❌-----It is not a valid temprature.-----❌")
             continue
         
-        temprature_in_kelvin = temprature_in_celsius + 273.15
-        temprature_in_fahrenheit = (temprature_in_celsius * (9 / 5)) + 32
-        print(f"\n{temprature_in_celsius} C equals to {temprature_in_kelvin} K and {temprature_in_fahrenheit} F.")
+        conversion = input("\nDo you want to convert it into K or F? ").lower()
+
+        if conversion == "k":
+            converted_temprature = temprature_in_celsius + 273.15
+            print(f"\n{temprature_in_celsius} C equals to {converted_temprature:.2f} K. 🌡️\n")
+        elif conversion == "f":
+            converted_temprature = (temprature_in_celsius * (9 / 5)) + 32
+            print(f"\n{temprature_in_celsius} C equals to {converted_temprature:.2f} F. 🌡️\n")
+        else:
+            print("\nNot possible unit.❌\n")
         
         again = input("Do you want to convert temprature again? ")
         if again != "y":
+            print("\nThanks! for using temprature converter. ❤️")
             break
 
 
