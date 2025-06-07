537b5d38090dfede5498f01349b27a372d1d998c
abs <abdulsalam.network@gmail.com>
2025-06-07 10:50:46 +0500

    Improve code

--- temprature_converter/temprature.py
+++ temprature_converter/temprature.py
@@ -6,13 +6,17 @@ def main() -> None:
 
     while True:
         try:
-            temprature_in_celsius = float(input("\nEnter temprature: "))
+            temprature_in_celsius = float(input("\nEnter temprature in Celsius: "))
         except ValueError:
-            print("\n-----It is not a valid temprature.-----")
-        else:
-            temprature_in_kelvin = temprature_in_celsius + 273.15
-            temprature_in_fahrenheit = (temprature_in_celsius * (9 / 5)) + 32
-            print(f"\n{temprature_in_celsius} C equals to {temprature_in_kelvin} K and {temprature_in_fahrenheit} F.")
+            print("\n❌-----It is not a valid temprature.-----❌")
+            continue
+        
+        temprature_in_kelvin = temprature_in_celsius + 273.15
+        temprature_in_fahrenheit = (temprature_in_celsius * (9 / 5)) + 32
+        print(f"\n{temprature_in_celsius} C equals to {temprature_in_kelvin} K and {temprature_in_fahrenheit} F.")
+        
+        again = input("Do you want to convert temprature again? ")
+        if again != "y":
             break
 
 
